04c72178e71c3d52a96588467b0339e150833182
nhashemi202 <60355451+nhashemi202@users.noreply.github.com>
2023-07-11 15:33:09 +0330

    Add SIR method

--- main.py
+++ main.py
@@ -1,8 +1,58 @@
 import numpy as np
 from numpy import linalg as LA
 
+def smoothed_isotonic_regression(n = 1000, lambda_ = 1, rho = 0.1, num_iters = 100):
+    """
+    :param n: number of predictors
+    :param lambda_: penalty parameter
+    :param rho: lagrangian coefficient
+    :param num_iters: number of iterations of the algorithm
+    """
+    # variable initialization
+    q = np.zeros((n - 1,))
+    p = np.zeros((n - 1,))
+    y1 = np.zeros((n - 1,))
+    y2 = np.zeros((n - 2,))
+    x = np.random.randint(low=0, high=1000, size=n)
+    w = np.ones((n,))
 
-def multidimensional_ordering(E_1, E_2, Y, W, rho = 1, num_iters=100):
+    k = 0
+    p_next = np.zeros((n - 1,))
+    q_next = np.zeros((n - 1,))
+    r2_next = np.zeros((n - 2,))
+
+    # dual and primal residuals
+    ss, rs = [], []
+
+    while k < num_iters:
+
+        u_next = np.maximum((rho * (q - p) - y1)/(rho + 2 * lambda_), np.zeros_like(p))
+        p_next[0] = (2 * w[0] * x[0] + rho * q[0] - rho * u_next[0] - y1[0]) / (2 * w[0] + rho)
+        p_next[1:] = (w[1:n-1] * x[1:n-1]+ rho * q[1:] - rho * u_next[1:] - y1[1:] \
+                      + rho * q[0:n-2] - y2[0:n-2])/(w[1:n-1] + 2 * rho)
+        q_next[:n-2] = (w[1:n-1] * x[1:n-1] + rho * p_next[:n-2] + rho * u_next[:n-2] \
+                        + y1[:n-2] + rho * p_next[1:n-1] + y2[:n-2])/(w[1:n-1] + 2 * rho)
+        q_next[-1] = (2 * w[-1] * x[-1] + rho * p_next[-1] + u_next[-1])/(2 * w[-1] + rho)
+        r1_next = p_next - q_next + u_next
+        r2_next = p_next[1:] - q_next[:n-2]
+        s1_next = rho * (p_next - q_next - p + q)
+        s2_next = rho * (q - q_next)
+        r_next = np.sqrt(np.linalg.norm(r1_next)**2 + np.linalg.norm(r2_next)**2)
+        s_next = np.sqrt(np.linalg.norm(s1_next)**2 + np.linalg.norm(s2_next)**2)
+        y1_next = y1 + rho * r1_next
+        y2_next = y2 + rho * r2_next
+
+        p, q, u, y1, y2 = p_next, q_next, u_next, y1_next, y2_next
+
+        k += 1
+
+        ss.append(s_next)
+        rs.append(r_next)
+
+    return p, q, u, rs, ss
+
+
+def multidimensional_ordering(E_1, E_2, Y, W, rho = 1, num_iters = 100):
     """
 
     :param E_1: representation of edge set E (|E|, n)
